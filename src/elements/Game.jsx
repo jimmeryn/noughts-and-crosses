2356cd626f6e1838c16a2932815f17acc3dee539
jimmeryn <jimmeryn@gmail.com>
2019-11-23 22:43:29 +0100

    Update css & game

--- src/elements/Game.jsx
+++ src/elements/Game.jsx
@@ -7,10 +7,11 @@ import Board from "./Board";
  */
 const Game = () => {
   let [size, setSize] = useState(0);
-  let [squares, setSquares] = useState(Array(size * size).fill(null));
-  let [history, setHistory] = useState([{ squares }]);
-  let [playerTurn, setPlayerTurn] = useState(true);
-  let [stepNumber, setStepNumber] = useState(0);
+  const [squares, setSquares] = useState(Array(size * size).fill(null));
+  const [history, setHistory] = useState([{ squares }]);
+  const [playerTurn, setPlayerTurn] = useState(null);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [status, setStatus] = useState("");
 
   const getCurrent = () => {
     return history[stepNumber].squares;
@@ -31,6 +32,11 @@ const Game = () => {
     setSquares(squares);
     setStepNumber(newHistory.length);
     setPlayerTurn(!playerTurn);
+    console.log(checkStatus());
+    checkStatus() === null
+      ? setStatus("Next player: " + (!playerTurn ? "X" : "O"))
+      : checkStatus();
+    console.log(status);
   };
 
   /**
@@ -77,14 +83,15 @@ const Game = () => {
   const handleClickVisibility = () => {
     setSize = null;
     sheet.insertRule("span {visibility: hidden");
+    setPlayerTurn(true);
+    setStatus("Next player: " + (!playerTurn ? "X" : "O"));
   };
 
   /**
    *  Calculating if winners occurs.
-   *   @param {Array} squares
+   *   @param {Array} squares - current board
    */
   const calculateWinner = squares => {
-    //change that to recursive check
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -96,25 +103,6 @@ const Game = () => {
       [2, 4, 6]
     ];
 
-    // console.log(squares);
-    ///
-    // let result = [];
-    // for (let x = 0; x < size; x++) {
-    //   for (let i = size * x; i < size * x + size - 2; ++i) {
-    //     for (let j = 0; j < 3; j++) {
-    //       result.push(squares[i + j]);
-    //     }
-    //     if (
-    //       result[0] !== undefined &&
-    //       result.every(e => e === result[0]) === true
-    //     ) {
-    //       return result[0];
-    //     }
-    //     result = [];
-    //   }
-    // }
-    ///
-
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (
@@ -124,37 +112,30 @@ const Game = () => {
       ) {
         return squares[a];
       }
+      // return squares.findIndex(e => e === undefined) !== -1
+      //   ? "draw"
+      //   : undefined;
     }
-
-    // if (
-    //   squares.filter(e => e === "X" || "O").length === squares.length &&
-    //   squares.length >= 3
-    // ) {
-    //   return `draw`;
-    // }
-    return null;
   };
 
   /**
    * Using checkWinner function to create status message.
-   *  @returns {String} Winner X
-   *  @returns {String} Winner O
-   *  @returns {String} Draw
-   *  @returns {null}   Game keeps going
+   *  @return {String} string - Winner X
+   *  @return {String} string - Winner O
+   *  @return {String} string - Draw
+   *  @return {null}   null - Game keeps going
    */
   const checkStatus = () => {
     const winner = calculateWinner(squares);
-    let status;
     if (winner === "X" || winner === "O") {
-      status = `Winner ${winner}`;
+      setStatus(`Winner ${winner}`);
     } else {
       if (winner === "draw") {
-        status = `draw`;
+        setStatus(`Draw`);
       } else {
-        status = "Next player: " + (playerTurn ? "X" : "O");
+        setStatus("Next player: " + (!playerTurn ? "X" : "O"));
       }
     }
-    return status;
   };
 
   /**
@@ -176,31 +157,44 @@ const Game = () => {
   let content = (
     <div className="game">
       <div className="game-board">
-        <span>
-          {"Set board size (3-10): "}
-          <input
-            type="phone"
-            style={{
-              width: "30px"
-            }}
-            onChange={handleChange}
+        {typeof playerTurn !== "boolean" ? (
+          <span>
+            {"Set board size (3-10): "}
+            <input
+              type="phone"
+              style={{
+                width: "30px"
+              }}
+              onChange={handleChange}
+            />
+            {size ? (
+              <button onClick={() => handleClickVisibility()}>
+                Start game
+              </button>
+            ) : (
+              <button disabled>Start game</button>
+            )}
+          </span>
+        ) : (
+          <div>{status}</div>
+        )}
+        {playerTurn != null ? (
+          <Board
+            size={size}
+            squares={getCurrent()}
+            setSquares={setSquares}
+            playerTurn={playerTurn}
+            setPlayerTurn={setPlayerTurn}
+            calculateWinner={calculateWinner}
+            handleClick={i => handleClick(i)}
           />
-          <button onClick={() => handleClickVisibility()}>Start game</button>
-        </span>
-        <Board
-          size={size}
-          squares={getCurrent()}
-          setSquares={setSquares}
-          playerTurn={playerTurn}
-          setPlayerTurn={setPlayerTurn}
-          calculateWinner={calculateWinner}
-          handleClick={i => handleClick(i)}
-        />
-      </div>
-      <div className="game-info">
-        <br />
-        <div>{checkStatus()}</div>
-        <ol>{renderMoves()}</ol>
+        ) : null}
+        {playerTurn != null ? (
+          <div className="game-info">
+            <br />
+            <ol>{renderMoves()}</ol>
+          </div>
+        ) : null}
       </div>
     </div>
   );
